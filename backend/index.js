fc95bf1f9913fdeefc4acd4884c7e952a9befa40
Mohamed Khaled <mohamed.kmohamed@miu-net.local>
2024-10-19 11:37:56 +0300

    updates on cors

--- backend/index.js
+++ backend/index.js
@@ -6,21 +6,35 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const taskRouter = require("./routers/taskRouter");
 const userRouter = require("./routers/userRouter");
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.options("*", cors()); // Allow preflight requests for all routes
-
-app.use(morgan("dev"));
 dotenv.config();
 const dbConnect = require("./config/dbConnect");
 
+// CORS configuration
+const allowedOrigins = [
+  "https://taskhub-task-management-app-xmidos25256gmailcoms-projects.vercel.app",
+]; // Add your frontend URL here
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // Allow cookies to be sent
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    optionsSuccessStatus: 204, // For legacy browser support
+  })
+);
+
+app.use(morgan("dev"));
+
+// Routes
 app.use("/api/tasks", taskRouter);
 app.use("/api/users", userRouter);
 
 app.get(`/`, (req, res) => {
   res.send("Hello world");
 });
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
