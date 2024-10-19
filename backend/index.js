dc2953a108b27d9bcaf8410ef9eed369de46ff5a
Mohamed Khaled <mohamed.kmohamed@miu-net.local>
2024-10-19 11:27:22 +0300

    updates on cors

--- backend/index.js
+++ backend/index.js
@@ -8,29 +8,22 @@ const taskRouter = require("./routers/taskRouter");
 const userRouter = require("./routers/userRouter");
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*", // Allow all origins
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true, // If you need cookies/authentication
-  })
-);
+app.use(cors());
+app.options("*", cors()); // Allow preflight requests for all routes
+
 app.use(morgan("dev"));
 dotenv.config();
 const dbConnect = require("./config/dbConnect");
 
-dbConnect();
 app.use("/api/tasks", taskRouter);
 app.use("/api/users", userRouter);
-app.get("/test", (req, res) => {
-  res.send("Testing server");
-});
+
 app.get(`/`, (req, res) => {
   res.send("Hello world");
 });
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
+  dbConnect();
   console.log(`Server is running on port : http://localhost:${port}/`);
 });
